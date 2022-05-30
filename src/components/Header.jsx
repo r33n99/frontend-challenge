2c3f0932369380186f0e2b7b55db8121958e91fc
r33n99 <rinni499@gmail.com>
2022-05-30 15:25:22 +0600

    editV1.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <div className="header">
-        <Link to="/">Все котики</Link>
-        <Link to="/favorites">Любимые котики</Link>
+        <NavLink className='header__link' activeClassName={"active"} to="/">Все котики</NavLink>
+        <NavLink className='header__link' activeClassName={"active"} to="/favorites">Любимые котики</NavLink>
     </div>
   )
 }
