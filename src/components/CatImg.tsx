3becc61ad352fead627eaabb5b26c5854fb567a3
r33n99 <rinni499@gmail.com>
2022-06-01 16:01:06 +0600

    addDoubleClickForDeleteFavorites

--- src/components/CatImg.tsx
+++ src/components/CatImg.tsx
@@ -17,17 +17,20 @@ export const CatImg: FC<CatImgProps> = ({ url, id, isFavoritePage }) => {
   const { favoritesCats } = useAppSelector(selectState);
   const dispatch = useAppDispatch();
 
-  const handleClick = () => {
-    isFavoritePage
+  const handleClick = (e: React.UIEvent<EventTarget>) => {
+    isFavoritePage // если находимся на странице избранного выполняем только удаление
       ? dispatch(deleteFavoriteCat(id))
       : dispatch(addFavoriteCat(id));
+    if (e.detail === 2) {
+      dispatch(deleteFavoriteCat(id)); // при двойном клике удаляем из избранного
+    }
   };
   return (
     <div className="cat">
       <img className="cat__img" src={url} alt="cat" />
       <div className="cat__heart">
         <img
-          onClick={() => handleClick()}
+          onClick={(e) => handleClick(e)}
           src={favoritesCats.find((el) => el.id === id) ? favorite : heart}
           alt="asd"
         />
